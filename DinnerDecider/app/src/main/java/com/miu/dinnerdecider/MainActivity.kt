7688f10ca1ac35555e032e838dd9d1f395a6bb2a
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2021-09-28 17:48:32 -0500

    complete dinner decider

--- DinnerDecider/app/src/main/java/com/miu/dinnerdecider/MainActivity.kt
+++ DinnerDecider/app/src/main/java/com/miu/dinnerdecider/MainActivity.kt
@@ -2,10 +2,35 @@ package com.miu.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputText:EditText
+    private lateinit var decision:TextView
+    var foods = Foods()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputText = findViewById<EditText>(R.id.input_add)
+        decision = findViewById<TextView>(R.id.food_text)
+    }
+
+    fun addFood(view: View) {
+        val text = inputText.text.toString()
+        if (text.isNotEmpty()) {
+            foods.addFood(text)
+            inputText.text.clear()
+            val toast = Toast.makeText(applicationContext,"Added $text", Toast.LENGTH_LONG)
+            toast.show();
+        }
+    }
+
+    fun decideFood(view: View) {
+        decision.text = foods.getRandomFood()
     }
 }
