2f0e37f6c7b96fab17e066c93fbe85d4b0d39a71
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2021-10-11 00:27:04 -0500

    remove cv project

--- CurriculumVitae/app/src/main/java/com/miu/curriculumvitae/common/Person.kt
+++
@@ -1,6 +0,0 @@
-package com.miu.curriculumvitae.common
-
-import java.io.Serializable
-
-class Person(var avatar: Int, var firstName: String, var lastName: String, var email: String, var profession: String, var phoneNumber: String, var about: String) : Serializable {
-}
